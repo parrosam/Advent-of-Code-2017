b3ed2f0aa28dfb6c0b02b8c804efd22cb8b15fc2
Samuel Parrotte <samuel.parrotte@gmail.com>
2017-12-02 23:48:10 -0800

    Solve puzzle 2

--- day2.py
+++ day2.py
@@ -1,6 +1,7 @@
 """Day 2 of Advent of Code 2017"""
 
 import utils
+import itertools
 
 def get_input():
     """Get input2.txt data as a list of list of int"""
@@ -14,10 +15,8 @@ def get_input():
 
     return line_list
 
-def puzzle_1():
+def puzzle_1(data):
     """Puzzle 1 solution"""
-    data = get_input()
-
     check_sum = 0
     for line in data:
         line_range = get_line_range(line)
@@ -37,5 +36,28 @@ def get_line_range(line):
 
     return largest - smallest
 
+def puzzle_2(data):
+    """Puzzle 2 solution"""
+    total = 0
+    for line in data:
+        total += find_clean_division(line)
+
+    return total
+
+def find_clean_division(line):
+    """Find tuple that has clean division"""
+    combinations = itertools.combinations(line, 2)
+    pairs = [combination for combination in combinations
+             if combination[0] % combination[1] == 0
+             or combination[1] % combination[0] == 0]
+
+    pair = pairs[0]
+    if pair[0] > pair[1]:
+        return pair[0] // pair[1]
+    else:
+        return pair[1] // pair[0]
+
 if __name__ == '__main__':
-    print(puzzle_1())
+    DAY_2_INPUT = get_input()
+    print(puzzle_1(DAY_2_INPUT))
+    print(puzzle_2(DAY_2_INPUT))
