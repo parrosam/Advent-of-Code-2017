56702ed660743ffee76fdf04b4530ddc8f674d8d
Samuel Parrotte <samuel.parrotte@gmail.com>
2017-12-02 16:17:15 -0800

    Complet puzzle 2

--- day1.py
+++ day1.py
@@ -8,12 +8,16 @@ def get_file_contents(file_loc):
     with open(file_loc, 'r') as file_to_read:
         return file_to_read.read()
 
-def puzzle_1():
-    """Main method"""
-    input_file_loc = "data/input1-1.txt"
+def prep_input_list():
+    """Read input from text file and convert to list of ints"""
+    input_file_loc = "data/input1.txt"
     input_text = get_file_contents(input_file_loc)
 
-    nums = list(map(int, input_text))
+    return list(map(int, input_text))
+
+def puzzle_1():
+    """Puzzle 1"""
+    nums = prep_input_list()
     length = len(nums)
 
     total = 0
@@ -26,5 +30,18 @@ def puzzle_1():
 
     return total
 
+def puzzle_2():
+    """Puzzle 2"""
+    nums = prep_input_list()
+    length = len(nums)
+
+    total = 0
+    for i in range(length):
+        halfway = (i + length//2) % length
+        if nums[i] == nums[halfway]:
+            total += nums[i]
+
+    return total
+
 if __name__ == '__main__':
-    print(puzzle_1())
+    print(puzzle_2())
